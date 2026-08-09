afbdf55220706dc8b1bc1da1b6c08432a756bd04
GeethaParasuraman <122171945+GeethaParasuraman@users.noreply.github.com>
2026-08-09 23:37:57 +0530

    Week 1 assignments - Primitive data type

---
+++ Week 1/PrimitiveDataType.js
@@ -0,0 +1,25 @@
+// JavaScript Data Types Assignment
+
+// Declare variables using let
+let userName = "Geetha";
+let organization = "My Organization";
+let contactNumber = 9876543210;
+let knowsAutomation = true;
+let usesPlaywright;
+
+// Display values and their data types
+
+console.log("userName:", userName);
+console.log("Type of userName:", typeof userName);
+
+console.log("organization:", organization);
+console.log("Type of organization:", typeof organization);
+
+console.log("contactNumber:", contactNumber);
+console.log("Type of contactNumber:", typeof contactNumber);
+
+console.log("knowsAutomation:", knowsAutomation);
+console.log("Type of knowsAutomation:", typeof knowsAutomation);
+
+console.log("usesPlaywright:", usesPlaywright);
+console.log("Type of usesPlaywright:", typeof usesPlaywright);
